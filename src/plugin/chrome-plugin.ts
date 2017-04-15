481815560659196546e476e50f17fd9a9d6a904e
Williams Medina <williams.medinaa@gmail.com>
2017-04-14 21:22:14 -0400

    chore: update library

--- src/plugin/chrome-plugin.ts
+++ src/plugin/chrome-plugin.ts
@@ -25,49 +25,54 @@ export class ChromePlugin extends ChromeDebuggingProtocolPlugin {
   }
 
   async didRun () {
-    this.pluginClient.status.startLoading()
-    this.pluginClient.status.update('Running Chrome')
-    this.pluginClient.console.clear()
-    let options = await this.pluginClient.getOptions()
-    // run chrome
-    this.launcher.hostName = 'localhost'
-    this.launcher.portNumber = options.portNumber
-    if (options.binaryPath === BinaryType.Custom) {
-      this.launcher.customBinaryPath = options.customBinaryPath
-    } else {
-      this.launcher.customBinaryPath = null
-    }
-    let projectPath = this.pluginClient.getPath()
-    let contextPath = join(projectPath, options.basePath)
-    this.debugger.basePath = projectPath
-    this.debugger.serverUrl = options.serverUrl
+    try {
+      this.pluginClient.status.startLoading()
+      this.pluginClient.status.update('Running chrome')
+      this.pluginClient.console.clear()
+      let options = await this.pluginClient.getOptions()
+      // run chrome
+      this.launcher.hostName = 'localhost'
+      this.launcher.portNumber = options.portNumber
+      if (options.binaryPath === BinaryType.Custom) {
+        this.launcher.customBinaryPath = options.customBinaryPath
+      } else {
+        this.launcher.customBinaryPath = null
+      }
+      let projectPath = this.pluginClient.getPath()
+      let contextPath = join(projectPath, options.basePath)
+      this.debugger.basePath = projectPath
+      this.debugger.serverUrl = options.serverUrl
 
-    this.debugger.setMappings({})
-    this.debugger.addMapping(options.serverUrl, contextPath)
-    this.debugger.addMapping(contextPath, options.serverUrl)
-    let defaultMappings = {
-      'webpack:///': '.',
-      'webpack:///~/': './node_modules'
-    }
-    // add defined mappings
-    Object.assign(defaultMappings, options.mappingPaths)
-    Object
-      .keys(defaultMappings)
-      .forEach((origin) => {
-        this.debugger.addMapping(origin, normalize(join(projectPath, '/', defaultMappings[origin], '/')))
-      })
+      this.debugger.setMappings({})
+      this.debugger.addMapping(options.serverUrl, contextPath)
+      this.debugger.addMapping(contextPath, options.serverUrl)
+      let defaultMappings = {
+        'webpack:///': '.',
+        'webpack:///~/': './node_modules'
+      }
+      // add defined mappings
+      Object.assign(defaultMappings, options.mappingPaths)
+      Object
+        .keys(defaultMappings)
+        .forEach((origin) => {
+          this.debugger.addMapping(origin, normalize(join(projectPath, '/', defaultMappings[origin], '/')))
+        })
 
-    this.launcher.url = options.serverUrl
-    this.disableConsole()
-    let socketUrl = await this.launcher.start()
-    if (socketUrl) {
-      this.pluginClient.status.update('Connecting to Debugger')
-      this.pluginClient.run()
-      await this.debugger.connect(socketUrl)
-      this.pluginClient.status.update('Debugger Attached')
+      this.launcher.url = options.serverUrl
+      this.disableConsole()
+      let socketUrl = await this.launcher.start()
+      if (socketUrl) {
+        this.pluginClient.status.update('Connecting to Debugger')
+        this.pluginClient.run()
+        await this.debugger.connect(socketUrl)
+        this.pluginClient.status.update('Debugger Attached')
+        this.pluginClient.status.stopLoading()
+        this.enableConsole()
+        // await this.debugger.domains.Page.reload()
+      }
+    } catch (e) {
+      this.pluginClient.status.update(e)
       this.pluginClient.status.stopLoading()
-      this.enableConsole()
-      // await this.debugger.domains.Page.reload()
     }
   }
 }
